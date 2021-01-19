afa6a2fd23a7072008cff96b31ecd535bf9394d6
nabla27 <63175080+nabla27@users.noreply.github.com>
2021-01-19 23:49:17 +0900

    rpg:初期設定 キャラクターカーソルの設定 マップ描写の関数化

--- RPG/rpg.cpp
+++ RPG/rpg.cpp
@@ -13,28 +13,48 @@ enum class MAP { LOCAL, EAST1 }; MAP map;
 enum class BUILD { HOME, CAVE1, TOWER }; BUILD build;
 enum class CHARA { ENEMY1, ENEMY2, ENEMY3 }; CHARA chara;
 
+int cursorX = 30;
+int cursorY = 15;
+
+
+void disp_local() {
+	system("cls");
+	for (int y = 0; y < FIELD_HIGHT; y++) {
+		for (int x = 0; x < FIELD_WIDTH; x++) {
+			if (x == cursorX && y == cursorY) {
+				printf("—");
+			}
+			else if (map_local[y][x] == 0) {
+				printf("E");
+			}
+			else if (map_local[y][x] == 1) {
+				printf("ƒ©");
+			}
+		}
+		printf("\n");
+	}
+}
 
 
 
 int main() {
 	while (1){
 		if (mode == MODE::GAME) {
+			time_t replot = time(NULL) - 5;
 			while (1) {
-				time_t replot = time(NULL);
-				if (replot < time(NULL)) {
+				if (replot  + 5 < time(NULL)) {
 					replot = time(NULL);
-					system("cls");
-					for (int y = 0; y < FIELD_HIGHT; y++) {
-						for (int x = 0; x < FIELD_WIDTH; x++) {
-							if (map_local[y][x] == 0) {
-								printf("E");
-							}
-							else if (map_local[y][x] == 1) {
-								printf("ƒ©");
-							}
-						}
-						printf("\n");
+					disp_local();
+				}
+
+				if (_kbhit()) {
+					switch (_getch()) {
+					case 'w':cursorY--; break;
+					case 'a':cursorX--; break;
+					case 's':cursorY++; break;
+					case 'd':cursorX++; break;
 					}
+					disp_local();
 				}
 			}
 		}
