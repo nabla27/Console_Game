cb34e6f3637ba76739de05a12ba0b0bca0a01a2a
nabla27 <63175080+nabla27@users.noreply.github.com>
2021-01-20 00:12:24 +0900

    rpg:キャラ移動 キャラ移動の当たり判定、条件を追加(最端、山)

--- RPG/rpg.cpp
+++ RPG/rpg.cpp
@@ -16,6 +16,10 @@ enum class CHARA { ENEMY1, ENEMY2, ENEMY3 }; CHARA chara;
 int cursorX = 30;
 int cursorY = 15;
 
+//===============================================
+//マップ配列を扱う時、x,y座標が逆になることに注意
+//===============================================
+
 
 void disp_local() {
 	system("cls");
@@ -35,6 +39,17 @@ void disp_local() {
 	}
 }
 
+bool move_local(int x, int y) {
+	if (x<0 || x>FIELD_WIDTH - 1 || y<0 || y>FIELD_HIGHT - 1) {     //マップの外にはいけない
+		return false;
+	}
+	if (map_local[y][x] == 1) {										//山には行けない
+		return false;
+	}
+	else
+		return true;
+}
+
 
 
 int main() {
@@ -49,10 +64,10 @@ int main() {
 
 				if (_kbhit()) {
 					switch (_getch()) {
-					case 'w':cursorY--; break;
-					case 'a':cursorX--; break;
-					case 's':cursorY++; break;
-					case 'd':cursorX++; break;
+					case 'w':if (move_local(cursorX, cursorY - 1) == true) { cursorY--; }; break;
+					case 'a':if (move_local(cursorX - 1, cursorY) == true) { cursorX--; }; break;
+					case 's':if (move_local(cursorX, cursorY + 1) == true) { cursorY++; }; break;
+					case 'd':if (move_local(cursorX + 1, cursorY) == true) { cursorX++; }; break;
 					}
 					disp_local();
 				}
