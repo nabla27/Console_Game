662efd0db217fd537db879a295a56e970e414065
nabla27 <63175080+nabla27@users.noreply.github.com>
2021-01-19 23:28:00 +0900

    rpg:初期設定 マップのサイズ、描写の確認

--- RPG/rpg.cpp
+++ RPG/rpg.cpp
@@ -2,10 +2,40 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <time.h>
+#include "field_map.h"
+
+#define FIELD_WIDTH 60
+#define FIELD_HIGHT 29
+
+
+enum class MODE { GAME, MODE, SAVE, LOAD }; 
+MODE mode;
+enum class MAP { LOCAL, CAVE1 };
+
+
+
 
 int main() {
-	while (1) {
-		system("cls");
-		printf("Hello");
+	while (1){
+		if (mode == MODE::GAME) {
+			while (1) {
+				time_t replot = time(NULL);
+				if (replot < time(NULL)) {
+					replot = time(NULL);
+					system("cls");
+					for (int y = 0; y < FIELD_HIGHT; y++) {
+						for (int x = 0; x < FIELD_WIDTH; x++) {
+							if (map_local[y][x] == 0) {
+								printf("E");
+							}
+							else if (map_local[y][x] == 1) {
+								printf("ƒ©");
+							}
+						}
+						printf("\n");
+					}
+				}
+			}
+		}
 	}
 }
